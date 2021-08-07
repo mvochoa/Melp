bbef2af9009093c090e4df8347f09a659de76bbe
Mario Valentin Ochoa Mota <mvochoaa@gmail.com>
2021-08-07 12:17:01 -0500

    Add path to update restaurant

--- src/controllers/restaurant_controller.py
+++ src/controllers/restaurant_controller.py
@@ -5,10 +5,13 @@ from sqlalchemy.orm.session import Session
 
 from database.connection import get_db
 from models.restaurant_model import RestaurantModel
+from schemas.schema import Detail
 from schemas.restaurant_schema import ResponseRestaurant, RequestRestaurant
 
 router = APIRouter(
     prefix="/restaurants", tags=["Restaurantes"])
+
+responses_openapi = {404: {"model": Detail}}
 
 
 @router.post("", response_model=ResponseRestaurant, status_code=status.HTTP_201_CREATED, description="Registra nuevos restaurantes")
@@ -25,7 +28,16 @@ def list(db: Session = Depends(get_db)):
     return db.query(RestaurantModel).all()
 
 
-@router.get("/{id}", response_model=ResponseRestaurant, description="Muestra los datos de un restaurante en especifico")
+@router.get("/{id}", response_model=ResponseRestaurant, responses=responses_openapi, description="Muestra los datos de un restaurante en especifico")
 def show(id: str, db: Session = Depends(get_db)):
     restaurant = RestaurantModel.find(id, db)
     return restaurant.first()
+
+
+@router.put("/{id}", response_model=ResponseRestaurant, responses=responses_openapi, description="Actualiza los datos de un restaurante en especifico")
+def update(request: RequestRestaurant, id: str, db: Session = Depends(get_db)):
+    restaurant = RestaurantModel.find(id, db)
+    values = request.dict()
+    values['site'] = str(values['site'])
+    restaurant.update(values)
+    return RestaurantModel.find(id, db).first()
